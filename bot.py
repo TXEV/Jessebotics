53d3d5d41cbb06d6f0ab3cdc6c9b11132861b85e
Jesse <elgoogjessebotics@gmail.com>
2020-05-16 11:35:45 -0700

    test

--- bot.py
+++ bot.py
@@ -3,19 +3,11 @@ import json
 import os
 
 from discord.ext import commands
-from discord.ext.commands import has_permissions
-
-config = None # no value set until config.json is read
-all_cogs = None # no value set until all cogs are loaded
-
-# with open("private/config.json") as json_file:
-#     config = json.load(json_file)
 
 client = commands.Bot(command_prefix = "!")
 
 for filename in os.listdir('./cogs'):
     if filename.endswith(".py"):
-        all_cogs = filename[:-3]
         client.load_extension(f"cogs.{filename[:-3]}")
 
 #
@@ -29,11 +21,10 @@ async def on_ready():
 # Commands
 #
 @client.command()
-async def reload(ctx):
-    """Reloads all commands and updates bot."""
-    for extension in all_cogs:
-        client.unload_extension(f"cogs.{extension}")
-        client.load_extension(f"cogs.{extension}")
+async def reload(ctx, extension):
+    """Reloads a section of the commands and updates bot."""
+    client.unload_extension(f"cogs.{extension}")
+    client.load_extension(f"cogs.{extension}")
 
     print("Reloaded all commands")
 
