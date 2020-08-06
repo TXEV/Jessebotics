44e4938d6c2ff8af90f44845b0535152b497ad93
Jesse <elgoogjessebotics@gmail.com>
2020-08-06 15:35:10 -0700

    stuff

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-r = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q=Surrey,CA&appid=b76748d36e933cc435b3f697c5db3ff9")
-data = json.loads(r.text)
-current_weather = data["weather"][0]["main"]
-
-print(current_weather)
